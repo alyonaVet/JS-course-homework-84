df2c9c8e8b2f64cbecad01237873455bc4ba832b
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-10 11:43:05 +0600

    created TaskSchema; added Task model;

---
+++ api-todolist/models/Task.ts
@@ -0,0 +1,32 @@
+import mongoose, {Types} from 'mongoose';
+import User from './User';
+
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'User does not exist',
+    }
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  status: {
+    type: String,
+    required: true,
+    enum: ['new', 'in_progress', 'complete'],
+  }
+});
+
+const Task = mongoose.model('Task', TaskSchema);
+export default Task;
