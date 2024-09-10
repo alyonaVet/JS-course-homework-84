fd48fb08d8c51c79f419f80be79a27f1184166f3
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-10 15:38:11 +0600

    added tasksRouter;

--- api-todolist/index.ts
+++ api-todolist/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import config from './config';
 import usersRouter from './routers/users';
+import tasksRouter from './routers/tasks';
 
 const app = express();
 const port = 8000;
@@ -10,6 +11,7 @@ const port = 8000;
 app.use(express.json());
 app.use(cors(config.corsOptions));
 app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
 
 const run = async () => {
   await mongoose.connect(config.database);
